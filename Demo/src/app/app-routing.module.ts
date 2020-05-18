b3083ad135488d8ab9e0e66f4d99f1f875c6777e
Hansen Shi <Hansens@udel.edu>
2020-05-18 07:48:03 -0400

    landing page with sick background

--- Demo/src/app/app-routing.module.ts
+++ Demo/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
 {path: '', redirectTo: 'home', pathMatch: 'full'},
 {path: 'home', component: HomeComponent},
 {path: 'login', component: LoginComponent},
-{path: 'signup', component: SignupComponent}];
+{path: 'signup', component: SignupComponent},
+{path: 'landingPage', component: LandingPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Demo/src/app/app.module.ts
+++ Demo/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
     HomeComponent,
     LoginComponent,
     SignupComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
