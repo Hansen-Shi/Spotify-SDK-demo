d06c0588400fea1695d4075aa52f463b2e3f75fe
Derek Baum <dgbaum@udel.edu>
2020-05-18 11:26:21 -0400

    d

--- Demo/src/app/app-routing.module.ts
+++ Demo/src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-
 
 const routes: Routes = [
 {path: '', redirectTo: 'home', pathMatch: 'full'},
-{path: 'home', component: HomeComponent},
-{path: 'login', component: LoginComponent},
-{path: 'signup', component: SignupComponent},
-{path: 'landingPage', component: LandingPageComponent}];
+{path: 'home', component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
